b3b21131d18079c1f1111586aa2f3d2a4fd6fd50
Aadhi1111 <aadhishmoorthy@gmail.com>
2024-01-27 20:39:49 +0530

    js file for ques 3

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
-request.onload = funtion(){
+request.onload = function(){
     var res = JSON.parse(request.response);
-    console.log(res);
+    console.log(res); 
+    //Question 3 : solution
+    for(var i=0;i<res.length;i++){
+        //if(res[i].name.common){
+            console.log("Country Name : "+res[i].name.common +",Region : "+ res[i].region +",Sub Region : "+res[i].subregion +",Population : "+res[i].population);
+        //}
+        //else{
+           // console.log("NIL");
+        //}
+        
+    }
 }
 
 
